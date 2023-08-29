f8a3e41e64be13a49a5c60db457b2cc38d9b3311
Thamy Gois <thaamy_gois@hotmail.com>
2023-08-29 16:12:06 -0300

    Código finalizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        /*Deixei o número da conta e saldo como variáveis, pois são dados que, geralmente, o banco possui */
+        int numero_conta = 1021;
+        double saldo = 237.48;
+        
+        System.out.println("Olá! Por favor, digite o seu nome:");
+            String nomeCliente = scanner.next();
+
+        System.out.println("Insira seu sobrenome:");
+            String sobrenomeCliente = scanner.next();
+
+        System.out.println("Por favor, insira o número da agência que deseja abrir uma conta:");
+            String agencia = scanner.next();
+
+        
+        System.out.println("Agradecemos por abrir uma conta conosco, " + nomeCliente + " " + sobrenomeCliente);
+        System.out.println("Sua conta foi aberta na agência " + agencia + "E o número de sua conta é " + numero_conta);
+        System.out.println("Seu saldo de " + saldo + "já está disponível para sacar ou realizar Pix! Tenha um ótimo dia.");
+
+
+
     }
 }
